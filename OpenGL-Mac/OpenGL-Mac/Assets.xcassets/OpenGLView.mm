c61d072057ba67e50c5bbe0753d248f4b15570ec
陈威彪 <cwb2@meitu.com>
2020-02-19 19:27:55 +0800

    【feature】搭建openGL窗口

---
+++ OpenGL-Mac/OpenGL-Mac/Assets.xcassets/OpenGLView.mm
@@ -0,0 +1,19 @@
+//
+//  OpenGLView.m
+//  OpenGL-Mac
+//
+//  Created by william on 2020/2/19.
+//  Copyright © 2020 william. All rights reserved.
+//
+
+#import "OpenGLView.h"
+
+@implementation OpenGLView
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
---
+++ OpenGL-Mac/OpenGL-Mac/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OpenGL-Mac
+//
+//  Created by william on 2020/2/19.
+//  Copyright © 2020 william. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
