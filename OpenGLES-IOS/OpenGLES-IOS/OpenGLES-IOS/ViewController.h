d10b62eced7910926bfbfcdc27fd5d66746f88fe
陈威彪 <cwb2@meitu.com>
2020-02-22 18:16:38 +0800

    【Initialize】初始化工程

---
+++ OpenGLES-IOS/OpenGLES-IOS/OpenGLES-IOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLES-IOS
+//
+//  Created by william on 2020/2/22.
+//  Copyright © 2020 william. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OpenGLES-IOS/OpenGLES-IOS/OpenGLES-IOS/ViewController.mm
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  OpenGLES-IOS
+//
+//  Created by william on 2020/2/22.
+//  Copyright © 2020 william. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
