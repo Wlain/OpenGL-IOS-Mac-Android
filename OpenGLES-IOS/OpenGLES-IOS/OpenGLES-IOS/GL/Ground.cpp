4c4aa458d17a1f735b7c1f71ce5d361a545f9465
陈威彪 <cwb2@meitu.com>
2020-02-24 15:38:24 +0800

    【feature】修改顶点数据值，渲染地面

--- OpenGLES-IOS/OpenGLES-IOS/OpenGLES-IOS/GL/Ground.cpp
+++ OpenGLES-IOS/OpenGLES-IOS/OpenGLES-IOS/GL/Ground.cpp
@@ -18,23 +18,41 @@ Ground::~Ground() {
 void Ground::Initialize() {
     mVertexBuffer = new VertexBuffer();
     mShader = new Shader();
-    mVertexBuffer->SetSize(4);
-    mVertexBuffer->SetPosition(0, -1.0f, -1.0f, -5.0f);
-    mVertexBuffer->SetPosition(1,  1.0f, -1.0f, -5.0f);
-    mVertexBuffer->SetPosition(2, -1.0f,  1.0f, -5.0f);
-    mVertexBuffer->SetPosition(3,  1.0f,  1.0f, -5.0f);
-    mVertexBuffer->SetColor(0, 1.0f, 0.0f, 0.0f);
-    mVertexBuffer->SetColor(1, 0.0f, 1.0f, 0.0f);
-    mVertexBuffer->SetColor(2, 0.0f, 1.0f, 0.0f);
-    mVertexBuffer->SetColor(3, 0.0f, 0.0f, 1.0f);
+    mVertexBuffer->SetSize(1600);
+    for (int z = 0; z < 20; ++z) {
+        float zStart = 100.0f - z*10.0f;
+        for (int x = 0; x < 20; ++x) {
+            int offset = (x + z * 20) * 4;
+            float xStart = x*10.0f - 100.0f;
+            mVertexBuffer->SetPosition(offset, xStart, -1.0f, zStart);
+            mVertexBuffer->SetPosition(offset + 1, xStart + 10.0f, -1.0f, zStart);
+            mVertexBuffer->SetPosition(offset + 2, xStart, -1.0f, zStart - 10.0f);
+            mVertexBuffer->SetPosition(offset + 3, xStart + 10.0f, -1.0f, zStart - 10.0f);
+            mVertexBuffer->SetNormal(offset, 0.0f, 1.0f, 0.0f);
+            mVertexBuffer->SetNormal(offset + 1, 0.0f, 1.0f, 0.0f);
+            mVertexBuffer->SetNormal(offset + 2, 0.0f, 1.0f, 0.0f);
+            mVertexBuffer->SetNormal(offset + 3, 0.0f, 1.0f, 0.0f);
+            if ((z % 2) ^ (x % 2)) {
+                mVertexBuffer->SetColor(offset, 0.1f, 0.1f, 0.1f);
+                mVertexBuffer->SetColor(offset+1, 0.1f, 0.1f, 0.1f);
+                mVertexBuffer->SetColor(offset+2, 0.1f, 0.1f, 0.1f);
+                mVertexBuffer->SetColor(offset+3, 0.1f, 0.1f, 0.1f);
+            } else {
+                mVertexBuffer->SetColor(offset, 0.0f, 0.8f, 0.8f);
+                mVertexBuffer->SetColor(offset+1, 0.0f, 0.8f, 0.8f);
+                mVertexBuffer->SetColor(offset+2, 0.0f, 0.8f, 0.8f);
+                mVertexBuffer->SetColor(offset+3, 0.0f, 0.8f, 0.8f);
+            }
+        }
+    }
     mShader->Initialize("Resource/Shader/ground.vert", "Resource/Shader/ground.frag");
 }
-
-
 void Ground::Draw(glm::mat4 &viewProjectionMatrix) {
     glEnable(GL_DEPTH_TEST);
     mVertexBuffer->Bind();
     mShader->Bind(glm::value_ptr(viewProjectionMatrix));
-    glDrawArrays(GL_TRIANGLE_STRIP, 0 * 4, 4);
+    for (int i = 0; i < 400; ++i) {
+        glDrawArrays(GL_TRIANGLE_STRIP, i * 4, 4);
+    }
     mVertexBuffer->Unbind();
 }
